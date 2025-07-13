cd4a8ed0a1b5c90d39c2c11b598bea930fd0dd9f
Chandrakanta7377 <ckpahantasingh@gmail.com>
2025-07-13 23:43:51 +0530

    Update Prod.py

--- Prod.py
+++ Prod.py
@@ -1,14 +1,1 @@
-# prod.py
-import sys
-
-if len(sys.argv) != 3:
-    print("Usage: python prod.py <num1> <num2>")
-    sys.exit(1)
-
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-
-print("Addition: ", a + b)
-print("Subtraction: ", a - b)
-print("Multiplication: ", a * b)
-print("Division: ", a / b if b != 0 else "Cannot divide by zero")
+print("Hello pwskills")
